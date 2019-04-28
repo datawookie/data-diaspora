9fc110298a74bdab7c3f01fa50c70656aeb6cf7c
Andrew Collier <andrew@exegetic.biz>
2019-04-28 12:16:54 +0200

    Added World Air Passenger data from World Bank.

---
+++ scripts/worldbank-air-passengers.R
@@ -0,0 +1,22 @@
+# https://data.worldbank.org/indicator/is.air.psgr
+
+library(dplyr)
+library(tidyr)
+library(stringr)
+
+CSVFILE = "worldbank-air-passengers.csv"
+ZIPFILE = "worldbank-air-passengers.zip"
+RAWFILE = "API_IS.AIR.PSGR_DS2_en_csv_v2_10576669.csv"
+
+download.file("http://api.worldbank.org/v2/en/indicator/IS.AIR.PSGR?downloadformat=csv", ZIPFILE)
+
+unzip(ZIPFILE, RAWFILE)
+
+passengers <- read.csv(RAWFILE, skip = 4) %>%
+  select(-starts_with("Indicator")) %>%
+  rename_at(vars(starts_with("Country")), function(col) str_replace(col, "Country\\.", "") %>% str_to_lower()) %>%
+  gather(year, count, -name, -code) %>%
+  mutate(year = str_replace(year, "^X", "") %>% as.integer()) %>%
+  filter(!is.na(year))
+
+write.csv(passengers, CSVFILE, quote = FALSE, row.names = FALSE)
