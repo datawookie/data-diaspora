3705b03a976003e624067a8f9bb2147a043301e1
Andrew Collier <andrew@exegetic.biz>
2019-04-29 06:09:08 +0200

    Fixed format of CSV files.

--- scripts/metar.R
+++ scripts/metar.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(tidyr)
+library(stringr)
 
 tair <- read.csv("Tair.csv")
 icao <- read.csv("ICAO.csv")
@@ -15,8 +16,13 @@ missing <- tair %>%
 
 complete <- missing %>% filter(nan_count == 0) %>% pull(airport)
 
-icao <- icao %>% filter(icao %in% complete)
-tair <- tair %>% select_at(c("time", complete))
+icao <- icao %>%
+  filter(icao %in% complete) %>%
+  mutate(
+    name = str_squish(name)
+  )
+tair <- tair %>%
+  select_at(c("time", complete))
 
-write.csv(tair, "metar-air-temperature.csv")
-write.csv(icao, "metar-airports.csv")
+write.csv(tair, "metar-air-temperature.csv", quote = FALSE, row.names = FALSE)
+write.csv(icao, "metar-airports.csv", quote = FALSE, row.names = FALSE)
