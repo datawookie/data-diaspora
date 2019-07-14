481d84880169f50a69f887c74a75924a47e4e250
Andrew Collier <andrew@exegetic.biz>
2019-07-14 16:28:29 +0200

    Reduced scatter and added points.

--- scripts/height-age.R
+++ scripts/height-age.R
@@ -1,22 +1,21 @@
 library(dplyr)
 
-set.seed(13)
+set.seed(7)
 
 # Manufacture some feasible but fictional data.
 #
-N <- 100
+N <- 200
 #
 growing <- data.frame(
   age = runif(N, 0, 18) %>% round(2),
   gender = sample(c("M", "F"), N, replace = TRUE)
 ) %>% mutate(
-  height = ifelse(gender == "M", 25, 20) + ifelse(gender == "M", 8.9, 8.4) * age + rnorm(N, sd = 10),
+  height = ifelse(gender == "M", 25, 20) + ifelse(gender == "M", 8.9, 8.3) * age + rnorm(N, sd = 8),
   height = height %>% round(1)
 ) %>% select(age, gender, height)
-#
-# EXERCISES:
-#
-# 1. Explain what's going on in the code above.
-# 2. Produce a scatter plot of height versus age (use base graphics).
 
-write.csv(growing, "height-age.csv", row.names = FALSE, quote = FALSE)
+plot(height ~ age, col = gender, data = growing)
+
+summary(lm(height ~ age * gender, data = growing))
+
+write.csv(growing, here::here("height-age.csv"), row.names = FALSE, quote = FALSE)
