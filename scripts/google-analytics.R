dde56ed2f603cd30ad00d9850499d799b657a114
Andrew Collier <andrew@exegetic.biz>
2019-12-19 04:47:08 +0200

    Add script and data for Google Analytics

---
+++ scripts/google-analytics.R
@@ -0,0 +1,25 @@
+library(janitor)
+library(googleAnalyticsR)
+ga_auth()
+
+ga_account_list()
+
+ga_id <- 173110735 
+
+ga <- google_analytics_4(
+  ga_id, 
+  date_range = c("2018-12-01", "2019-12-01"),
+  metrics = c("sessions", "users"),
+  # https://developers.google.com/analytics/devguides/reporting/realtime/dimsmets/
+  dimensions = c(
+    "date",
+    "country",
+    "browser",
+    "operatingSystem"
+  ),
+  anti_sample = TRUE
+)
+
+ga <- ga %>% clean_names()
+
+write.csv(ga, "google-analytics.csv", quote = FALSE, row.names = FALSE)
