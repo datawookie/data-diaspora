1bc9a5ea6c51739791e4277d8c6a7f7b1afa29f9
Andrew Collier <andrew@exegetic.biz>
2020-02-09 13:18:10 +0200

    Country summary data

---
+++ scripts/country-summary.R
@@ -0,0 +1,30 @@
+library(rvest)
+library(janitor)
+library(dplyr)
+library(stringr)
+library(readr)
+
+FILE = "country-summary.csv"
+URL = "https://www.geonames.org/countries/"
+
+html <- read_html(URL)
+
+countries <- html %>% html_table() %>% .[[2]]
+
+countries <- countries %>%
+  clean_names %>%
+  rename(
+    iso_alpha2 = iso_3166alpha2,
+    iso_alpha3 = iso_3166alpha3,
+    iso_numeric = iso_3166numeric,
+    area = area_in_km
+  ) %>%
+  select(-capital) %>%
+  mutate(
+    continent = ifelse(is.na(continent), "NA", continent),
+    area = area %>% str_replace_all(",", "") %>% as.integer(),
+    population = population %>% str_replace_all(",", "") %>% as.integer()
+  )
+
+cat("# Data from ", URL, ".\n", sep = "", file = FILE)
+write_csv(countries, FILE, append = TRUE, na = "")
